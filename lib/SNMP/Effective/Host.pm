973d37568333b21fccabb8ce092f26d546064cd9
Jan Henning Thorsen <jhthorsen@jhthorsen.redpill-linpro.com>
2009-07-18 15:00:40 +0200

    add BUILD() which handles "varlist" argument.

--- lib/SNMP/Effective/Host.pm
+++ lib/SNMP/Effective/Host.pm
@@ -139,6 +139,23 @@ has fatal => (
 
 =head1 METHODS
 
+ $self->BUILD({ varlist => [...], ... });
+
+Called after C<new()>. See L<Moose::Object>.
+
+Sets up varlist.
+
+=cut
+
+sub BUILD {
+    my $self = shift;
+    my $args = shift;
+
+    if(my $varlist = $args->{'varlist'}) {
+        $self->add_varlist(@$varlist);
+    }
+}
+
 =head2 set_data
 
  $bool = $self->set_data([$oid, $iid, $value, $type], $ref_oid);
