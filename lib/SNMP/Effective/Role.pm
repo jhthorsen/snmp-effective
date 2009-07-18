451aed81237e582802494ec3f3850837ea5ec016
Jan Henning Thorsen <jhthorsen@jhthorsen.redpill-linpro.com>
2009-07-18 15:00:30 +0200

    move log() to Role.pm and improved functionality

--- lib/SNMP/Effective/Role.pm
+++ lib/SNMP/Effective/Role.pm
@@ -14,6 +14,8 @@ SNMP::Effective::Role - Common attributes and methods
 =cut
 
 use Moose::Role;
+use Log::Log4perl;
+use Data::Dumper ();
 use SNMP;
 
 =head1 OBJECT ATTRIBUTES
@@ -74,6 +76,15 @@ has callback => (
     default => sub { sub {} },
 );
 
+has _log => (
+    is => 'ro',
+    isa => 'Any',
+    default => sub {
+        Log::Log4perl->easy_init unless(Log::Log4perl->initialized);
+        Log::Log4perl->get_logger;
+    },
+);
+
 has _varlist => (
     traits => [qw/SNMP::Effective::AttributeHelpers::Trait::VarList/],
     provides => {
@@ -96,6 +107,39 @@ Add a list of VarBinds.
  $varbind_obj = $self->shift_varbind;
 
 Return one $varbind object.
+
+=head2 log
+
+ $bool = $self->log($level, $format, @args);
+ $bool = $self->log->$level($msg);
+
+Will log using L<Log::Log4perl>.
+
+=cut
+
+sub log {
+    return $_[0]->_log if(@_ == 1);
+
+    my $self   = shift;
+    my $level  = shift;
+    my $format = shift;
+    my @args;
+
+    return unless $self->_log->${ \"is_$level" };
+
+    local $Data::Dumper::Indent   = 0;
+    local $Data::Dumper::Terse    = 1;
+    local $Data::Dumper::Sortkeys = 1;
+
+    for(@_) {
+        push @args,
+              ref $_     ? Data::Dumper::Dumper($_)
+            : defined $_ ? $_
+            :              "__UNDEF__";
+    }
+
+    return $self->_log->$level(sprintf $format, @args);
+}
 
 =head1 BUGS
 
