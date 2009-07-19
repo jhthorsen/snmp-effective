7e4d8f0dae8cfb95d341af1ab0be2e835f6599ee
Jan Henning Thorsen <jhthorsen@jhthorsen.redpill-linpro.com>
2009-07-19 13:40:28 +0200

    SNMP::Effective::Callbacks are not a role anymore

--- lib/SNMP/Effective/Callbacks.pm
+++ lib/SNMP/Effective/Callbacks.pm
@@ -6,92 +6,88 @@ SNMP::Effective::Callbacks - SNMP callbacks
 
 =head1 DESCRIPTION
 
-This role contains callback methods for L<SNMP::Effective>. These methods
+This package contains callback methods for L<SNMP::Effective>. These methods
 are called from within an L<SNMP> get/getnext/set/... method and
 should handle the response from a SNMP client.
 
 =cut
 
-use Moose::Role;
+use strict;
+use warnings;
+use SNMP::Effective;
+use SNMP::Effective::Utils qw/:all/;
+
+=head1 CALLBACKS
 
 =head2 set
 
- $self->set($host, $request, $response);
+ set($snmp_effective, $host, $request, $response);
 
 This method is called after L<SNMP>.pm has completed it's C<set> call
 on the C<$host>.
 
 =cut
 
-sub set {
-    my $self     = shift;
-    my $host     = shift;
-    my $request  = shift;
-    my $response = shift;
+SNMP::Effective->add_snmp_callback(set => set => sub {
+    my($self, $host, $req, $res) = @_;
 
-    return $self->_end($host, 'Timeout') unless(ref $response);
+    return $self->_end($host, 'Timeout') unless(ref $res);
 
-    for my $r (grep { ref $_ } @$response) {
-        my $cur_oid = SNMP::Effective::make_numeric_oid($r->name);
+    for my $r (grep { ref $_ } @$res) {
+        my $cur_oid = make_numeric_oid($r->name);
         $host->set_data($r, $cur_oid);
     }
 
     return $self->_end($host);
-}
+});
 
 =head2 get
 
- $self->get($host, $request, $response);
+ get($snmp_effective, $host, $request, $response);
 
 This method is called after L<SNMP>.pm has completed it's C<get> call
 on the C<$host>.
 
 =cut
 
-sub get {
-    my $self     = shift;
-    my $host     = shift;
-    my $request  = shift;
-    my $response = shift;
+SNMP::Effective->add_snmp_callback(get => get => sub {
+    my($self, $host, $req, $res) = @_;
 
-    return $self->_end($host, 'Timeout') unless(ref $response);
+    return $self->_end($host, 'Timeout') unless(ref $res);
 
-    for my $r (grep { ref $_ } @$response) {
-        my $cur_oid = SNMP::Effective::make_numeric_oid($r->name);
+    for my $r (grep { ref $_ } @$res) {
+        my $cur_oid = make_numeric_oid($r->name);
         $host->set_data($r, $cur_oid);
     }
 
     return $self->_end($host);
-}
+});
 
 =head2 getnext
 
- $self->getnext($host, $request, $response);
+ getnext($snmp_effective, $host, $request, $response);
 
 This method is called after L<SNMP>.pm has completed it's C<getnext> call
 on the C<$host>.
 
 =cut
 
-sub getnext {
-    my $self     = shift;
-    my $host     = shift;
-    my $request  = shift;
-    my $response = shift;
+SNMP::Effective->add_snmp_callback(getnext => getnext => sub {
+    my($self, $host, $req, $res) = @_;
 
-    return $self->_end($host, 'Timeout') unless(ref $response);
+    return $self->_end($host, 'Timeout') unless(ref $res);
 
-    for my $r (grep { ref $_ } @$response) {
-        my $cur_oid = SNMP::Effective::make_numeric_oid($r->name);
+    for my $r (grep { ref $_ } @$res) {
+        my $cur_oid = make_numeric_oid($r->name);
         $host->set_data($r, $cur_oid);
     }
 
     return $self->_end($host);
-}
+});
 
 =head2 walk
 
- $self->walk($host, $request, $response);
+ walk($snmp_effective, $host, $request, $response);
 
 This method is called after L<SNMP>.pm has completed it's C<getnext> call
 on the C<$host>. It will continue sending C<getnext> requests, until an
@@ -99,26 +95,23 @@ OID branch is walked.
 
 =cut
 
-sub walk {
-    my $self     = shift;
-    my $host     = shift;
-    my $request  = shift;
-    my $response = shift;
-    my $i        = 0;
+SNMP::Effective->add_snmp_callback(walk => getnext => sub {
+    my($self, $host, $req, $res) = @_;
+    my $i = 0;
 
-    return $self->_end($host, 'Timeout') unless(ref $response);
+    return $self->_end($host, 'Timeout') unless(ref $res);
 
-    while($i < @$response) {
+    while($i < @$res) {
         my $splice = 2;
 
-        if(my $r = $response->[$i]) {
-            my($cur_oid, $ref_oid) = SNMP::Effective::make_numeric_oid(
-                                         $r->name, $request->[$i]->name
+        if(my $r = $res->[$i]) {
+            my($cur_oid, $ref_oid) = make_numeric_oid(
+                                         $r->name, $req->[$i]->name
                                      );
             $r->[0] = $cur_oid;
             $splice--;
 
-            if(defined SNMP::Effective::match_oid($cur_oid, $ref_oid)) {
+            if(defined match_oid($cur_oid, $ref_oid)) {
                 $host->set_data($r, $ref_oid);
                 $splice--;
                 $i++;
@@ -126,19 +119,19 @@ sub walk {
         }
 
         if($splice) {
-            splice @$request, $i, 1;
-            splice @$response, $i, 1;
+            splice @$req, $i, 1;
+            splice @$res, $i, 1;
         }
     }
 
-    if(@$response) {
-        $$host->getnext($response, [ \&_walk, $self, $host, $request ]);
+    if(@$res) {
+        $$host->getnext($res, [ \&_walk, $self, $host, $req ]);
         return;
     }
     else {
         return $self->_end($host);
     }
-}
+});
 
 =head1 DEBUGGING
 
