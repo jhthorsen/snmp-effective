c33164b9ceb819036b473475e52535d6925eb9c4
Jan Henning Thorsen <jhthorsen@cpan.org>
2010-10-27 11:51:43 +0200

    reformat++

--- examples/snmp_effective_example.pl
+++ examples/snmp_effective_example.pl
@@ -1,76 +1,55 @@
 #!/usr/bin/perl
 
-#==========================
-# snmp_effective_example.pl
-#==========================
+=head1 NAME
+
+snmp_effective_example.pl
+
+=cut
+
+sub say { print @_, "\n" } # use feature qw/say/;
+#BEGIN { $ENV{'SNMP_EFFECTIVE_DEBUG'} = 1 } # for debugging
 
 use warnings;
 use strict;
-use lib qw(./lib ../lib);
-use Log::Log4perl;
+use lib qw(lib ../lib);
 use SNMP::Effective;
 
-### set up Log::Log4perl, before SNMP::Effective do
-my $LOG4PERL = {
-    "log4perl.rootLogger"             => "TRACE, screen",
-    "log4perl.appender.screen"        => "Log::Log4perl::Appender::Screen",
-    "log4perl.appender.screen.layout" => "Log::Log4perl::Layout::SimpleLayout",
-};
-
-Log::Log4perl->init($LOG4PERL) unless(Log::Log4perl->initialized);
-
-### set up SNMP::Effective
+say 'Setting up SNMP::Effective...';
 my $effective = SNMP::Effective->new(
                     master_timeout => 5,
                     dest_host      => "127.0.0.1",
                     get            => "1.3.6.1.2.1.1.1.0", # sysDescr.0
                     getnext        => "sysName",
                     walk           => "sysUpTime",
-                    callback       => sub { my_callback(@_) },
+                    callback       => \&my_callback,
                     arg            => {
                         Version   => "2c",
                         Community => "public",
                     },
                 );
 
-### the end
+say 'SNMP::Effective->execute...';
 $effective->execute;
-exit 0;
 
-
-sub my_callback { #===========================================================
-
-    ### init
-    my $host  = shift;
+sub my_callback {
+    my $host = shift;
     my $error = shift;
-    my $heap  = $host->heap;
+    my $heap = $host->heap;
     my $data;
 
-    ### test for error
     if($error) {
-        say("Error: Could not get data from $host: $error");
+        say "Error: Could not get data from $host: $error";
         return;
     }
 
-    ### get data
     $data = $host->data;
 
-    ### print data
     for my $oid (keys %$data) {
-        say("-" x 78);
-        say("$host returned oid($oid) with data:");
-        say(join "\n", map {
-                             "\t$_ => $data->{$oid}{$_}";
-                         } keys %{ $data->{$oid} }
-        );
-        say();
+        say "-" x 78;
+        say "$host returned oid($oid) with data:";
+        say join "\n", map { "\t$_ => $data->{$oid}{$_}" } keys %{ $data->{$oid} };
+        say "";
     } 
 
-    ### the end
     return; # snmp-effective doesn't care about the return value
 }
-
-sub say { #===================================================================
-    print "$_\n" for(@_);
-}
-
