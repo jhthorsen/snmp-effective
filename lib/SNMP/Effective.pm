8573fd72d0920b2dfa701e23517afffdd3ee5307
Oliver Gorwits <oliver@cpan.org>
2016-05-18 14:50:49 +0100

    1.1103

--- lib/SNMP/Effective.pm
+++ lib/SNMP/Effective.pm
@@ -6,7 +6,7 @@ SNMP::Effective - An effective SNMP-information-gathering module
 
 =head1 VERSION
 
-1.1102
+1.1103
 
 =head1 SYNOPSIS
 
@@ -88,7 +88,7 @@ use POSIX qw(:errno_h);
 
 use base qw/ SNMP::Effective::Dispatch /;
 
-our $VERSION = '1.1102';
+our $VERSION = '1.1103';
 our %SNMPARG = (
     Version => '2c',
     Community => 'public',
